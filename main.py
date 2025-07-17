dc1cd777d18eb16b5d75d63e631d92ca46a6489d
Yair Hadad <kuyu12@gmail.com>
2025-07-17 18:16:09 +0300

    Add lambda func

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+
+
+def lambda_handler(event, context):
+    print("Received event:", json.dumps(event))
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Hello from Lambda Yair",
+            "input": event
+        })
+    }
+
+    return response
